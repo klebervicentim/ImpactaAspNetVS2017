3bf3d986b3ef7808369137c00d896f3e1a61cb97
Kleber Vicentim <kleber.vicentim@gmail.com>
2018-05-26 14:54:47 -0300

    Commit final (ou nao)

---
+++ Empresa.Mvc/Controllers/LoginController.cs
@@ -0,0 +1,72 @@
+﻿using Empresa.Mvc.Models;
+using Empresa.Repositorios.SqlServer;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.DataProtection;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+namespace Empresa.Mvc.Controllers
+{
+    public class LoginController : Controller
+    {
+        private readonly EmpresaDbContext _context;
+        private readonly IDataProtector _protectorProvider;
+
+        public LoginController(EmpresaDbContext context, IDataProtectionProvider protectionProvider, IConfiguration configuration)
+        {
+            _context = context;
+            _protectorProvider = protectionProvider.CreateProtector(configuration.GetSection("ChaveCriptografia").Value);
+        }
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index(LoginViewModel viewModel)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(viewModel);
+            }
+
+            var contato = _context.Contatos.SingleOrDefault
+                (c => c.Email == viewModel.Email && _protectorProvider.Unprotect(c.Senha) == viewModel.Senha);
+
+            if (contato == null)
+            {
+                ModelState.AddModelError(string.Empty, "Usuario ou senha incorretos");
+                return View(viewModel);
+            }
+
+            var claims = new List<Claim>
+            {
+                new Claim(ClaimTypes.Name, contato.Nome),
+                new Claim(ClaimTypes.Email, contato.Email),
+                new Claim(ClaimTypes.Role, "Admin"),
+                new Claim(ClaimTypes.Role, "Master"),
+                new Claim(ClaimTypes.Role, "Vendedor")
+            };
+
+            var identidade = new ClaimsIdentity(claims, CookieAuthenticationDefaults.AuthenticationScheme);
+            var principal = new ClaimsPrincipal(identidade);
+
+            HttpContext.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme, principal);
+            
+            return RedirectToAction("Index","Contatos");
+        }
+
+        public ActionResult Logout()
+        {
+            HttpContext.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
+            return RedirectToAction("Index", "Contatos");
+        }
+    }
+}
---
+++ Empresa.Mvc/Models/LoginViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Empresa.Mvc.Models
+{
+    public class LoginViewModel
+    {
+        [Required(ErrorMessage = "E-mail é obrigatório.")]
+        [DataType(DataType.EmailAddress)]
+        [Display(Name = "E-mail:")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Senha é obrigaória.")]
+        [DataType(DataType.Password)]
+        [Display(Name = "Senha:")]
+        public string Senha { get; set; }
+    }
+}
